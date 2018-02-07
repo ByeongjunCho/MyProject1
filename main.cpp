68c663781117355a754ed4bae76998e39c315aee
ByeongjunCho <34848770+ByeongjunCho@users.noreply.github.com>
2018-02-07 20:56:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,11 +38,8 @@ void test2()
 {
 	double a[] = { 0.5,1 };
 
-	int i;
-	i = 0;
-
-	double w00[3] = { 0.2,2,0 };
-	double w01[3] = { -0.1,1,0 };
+	int i = 0;
+	
 	int n_all = 2;
 	int n_model_inputs = 2;
 	double outputs[2];
@@ -72,13 +69,13 @@ void test2()
 }
 void test3()
 {
-	double a[] = { 0.5,1 };  // 입력 데이터
+	double a[] = { 0.5,1};  // 입력 데이터
 	double b[] = { 1,0 };   // 목표 행렬
 
 	int istart = 0;
 	int istop = 1;
 	int n_all = 2;
-	int n_all_weights = 8;
+	int n_all_weights = 12;
 	int n_model_inputs = 2;
 	double outputs[2];
 	int ntarg = 2;
@@ -89,26 +86,46 @@ void test3()
 	double h1[2];
 	double *hid_act[] = { h1 };
 	int max_neurons = 2;
-	double this_delta[2];  // 크기는 ntarg와 같음
+	double this_delta[3];  // 가장 많은 뉴런 개수 필요
+	double prior_delta[3]; 
+	double g1[6], g0[6];
+	double *grad_ptr[] = {g0,g1}; // 마지막 벡터의 크기=ntarg(출력 크기)
 
 	double final_layer_weigths[] = { 0.1,-0.1,0,0.3,0.4,0 }; // final_layer_weights[] = {첫 번째 결과 뉴런에 대한 가중치 + bias, 두 번째.... +bias}
-	int classifier = 0;
+	double grad[12];
+	int classifier = 1;
 	/*입력 = {0.5,1}, 출력 = {0.50,0.64}
 	가중치 w00,w01,w10,w11 = {0.2,-0.1,2,1}
 	마지막 레이어 가중치 = {0.1,0.2,-0.1,0.4}
 	각 뉴런 값 순서대로 히든레이어 = {0.9,0.72}*/
 
-	for (int i; i < 2; i++) {
+	for (int i=0; i < 2; i++) {
 		printf("%4.5f ", a[i]);
 	}
 	cout << endl;
-	trial_thr(a, n_all, n_model_inputs, outputs, ntarg, nhid_all, weigths_op, hid_act,
-		final_layer_weigths, classifier);
+	for (int i = 0; i < 3; i++) {
+		double error = batch_gradient(istart, istop, a, b, n_all, n_all_weights, n_model_inputs, outputs, ntarg, nhid_all, weigths_op, hid_act,
+			max_neurons, this_delta, prior_delta, grad_ptr, final_layer_weigths, grad, classifier);
 
-	for (int i = 0; i < 2; i++)
-		cout << outputs[i] << "  ";
+		cout << "출력  ";
+		for (int i = 0; i < 2; i++)
+			cout << outputs[i] << "  ";
+		cout << "error" << error << endl;
+	}
+	//cout << error << endl;
 }
+
 void main()
 {
-	test2();
+	test3();
+	/*int grad[] = { 3,4,3,2 };
+	int *a[] = { grad };
+	int *gg;
+	gg = a[0];
+	for (int i = 0; i < 4; i++)
+	{
+		*gg++ += 2*4;
+	}
+	for (int i = 0; i < 4; i++)
+		cout << grad[i] << endl;*/
 }
